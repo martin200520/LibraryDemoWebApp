44912a7c04451a728acef542683bc6ffa9243270
martin200520 <martin.gdlyan@gmail.com>
2024-03-24 02:57:33 +0400

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Library3.Data;
+using Library3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library3.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public BookController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> books = _context.Books.ToList();
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using Library3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library3.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
